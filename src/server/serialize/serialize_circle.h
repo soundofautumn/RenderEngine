aa4b534b3523be392b8a9c5609ebdbe3323bf75c
soundofautumn <qjm1412@163.com>
2024-09-19 12:32:11 +0800

    refactor(server): 删除pen_options

--- src/server/serialize/serialize_circle.h
+++ src/server/serialize/serialize_circle.h
@@ -17,12 +17,11 @@ boost::json::object serialize_circle(const Circle &circle) {
     if (std::holds_alternative<CircleUseCenterRadius>(circle)) {
         const auto &circle_ = std::get<CircleUseCenterRadius>(circle);
         return {{"type", "center_radius"}, {"center", serialize_point(circle_.center)},
-            {"radius", circle_.radius}, {"options", serialize_pen_options(circle_.options)}};
+            {"radius", circle_.radius}};
     } else if (std::holds_alternative<CircleUseThreePoints>(circle)) {
         const auto &circle_ = std::get<CircleUseThreePoints>(circle);
         return {{"type", "three_points"}, {"p1", serialize_point(circle_.p1)},
-            {"p2", serialize_point(circle_.p2)}, {"p3", serialize_point(circle_.p3)},
-            {"options", serialize_pen_options(circle_.options)}};
+            {"p2", serialize_point(circle_.p2)}, {"p3", serialize_point(circle_.p3)}};
     }
     return {};
 }
@@ -30,13 +29,11 @@ boost::json::object serialize_circle(const Circle &circle) {
 Circle deserialize_circle(const boost::json::object &obj) {
     if (obj.at("type").as_string() == "center_radius") {
         return CircleUseCenterRadius{.center = deserialize_point(obj.at("center").as_object()),
-            .radius = static_cast<int>(obj.at("radius").as_int64()),
-            .options = deserialize_pen_options(obj.at("options").as_object())};
+            .radius = static_cast<int>(obj.at("radius").as_int64())};
     } else if (obj.at("type").as_string() == "three_points") {
         return CircleUseThreePoints{.p1 = deserialize_point(obj.at("p1").as_object()),
             .p2 = deserialize_point(obj.at("p2").as_object()),
-            .p3 = deserialize_point(obj.at("p3").as_object()),
-            .options = deserialize_pen_options(obj.at("options").as_object())};
+            .p3 = deserialize_point(obj.at("p3").as_object())};
     }
     return {};
 }
@@ -46,12 +43,11 @@ boost::json::object serialize_arc(const Arc &arc) {
         const auto &arc_ = std::get<ArcUseCenterRadiusAngle>(arc);
         return {{"type", "center_radius_angle"}, {"center", serialize_point(arc_.center)},
             {"radius", arc_.radius}, {"start_angle", arc_.start_angle},
-            {"end_angle", arc_.end_angle}, {"options", serialize_pen_options(arc_.options)}};
+            {"end_angle", arc_.end_angle}};
     } else if (std::holds_alternative<ArcUseThreePoints>(arc)) {
         const auto &arc_ = std::get<ArcUseThreePoints>(arc);
         return {{"type", "three_points"}, {"p1", serialize_point(arc_.p1)},
-            {"p2", serialize_point(arc_.p2)}, {"p3", serialize_point(arc_.p3)},
-            {"options", serialize_pen_options(arc_.options)}};
+            {"p2", serialize_point(arc_.p2)}, {"p3", serialize_point(arc_.p3)}};
     }
     return {};
 }
@@ -61,13 +57,11 @@ Arc deserialize_arc(const boost::json::object &obj) {
         return ArcUseCenterRadiusAngle{.center = deserialize_point(obj.at("center").as_object()),
             .radius = static_cast<int>(obj.at("radius").as_int64()),
             .start_angle = static_cast<float>(obj.at("start_angle").as_double()),
-            .end_angle = static_cast<float>(obj.at("end_angle").as_double()),
-            .options = deserialize_pen_options(obj.at("options").as_object())};
+            .end_angle = static_cast<float>(obj.at("end_angle").as_double())};
     } else if (obj.at("type").as_string() == "three_points") {
         return ArcUseThreePoints{.p1 = deserialize_point(obj.at("p1").as_object()),
             .p2 = deserialize_point(obj.at("p2").as_object()),
-            .p3 = deserialize_point(obj.at("p3").as_object()),
-            .options = deserialize_pen_options(obj.at("options").as_object())};
+            .p3 = deserialize_point(obj.at("p3").as_object())};
     }
     return {};
 }
--- src/server/serialize/serialize_line.h
+++ src/server/serialize/serialize_line.h
@@ -14,14 +14,12 @@ using namespace RenderCore;
 
 boost::json::object serialize_line(const Line &line) {
     return {{"p1", serialize_point(line.p1)}, {"p2", serialize_point(line.p2)},
-        {"options", serialize_pen_options(line.options)},
         {"algorithm", static_cast<int64_t>(line.algorithm)}};
 }
 
 Line deserialize_line(const boost::json::object &obj) {
     return Line{.p1 = deserialize_point(obj.at("p1").as_object()),
         .p2 = deserialize_point(obj.at("p2").as_object()),
-        .options = deserialize_pen_options(obj.at("options").as_object()),
         .algorithm = static_cast<Line::LineAlgorithm>(obj.at("algorithm").as_int64())};
 }
 
