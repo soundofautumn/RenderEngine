c2f8b77c2412a96e67ea4553fb43dc40165a6226
soundofautumn <qjm1412@163.com>
2024-09-20 16:25:31 +0800

    feat: 添加多边形序列化的支持

--- src/server/serialize/serialize.cpp
+++ src/server/serialize/serialize.cpp
@@ -22,6 +22,10 @@ Primitive deserialize_primitive(const boost::json::object &primitive) {
         return deserialize_arc(primitive.at("Arc").as_object());
     } else if (primitive.contains("PenOptions")) {
         return deserialize_pen_options(primitive.at("PenOptions").as_object());
+    } else if (primitive.contains("Polygon")) {
+        return deserialize_polygon(primitive.at("Polygon").as_object());
+    } else if (primitive.contains("Rectangle")) {
+        return deserialize_rectangle(primitive.at("Rectangle").as_object());
     }
     return {};
 }
@@ -35,6 +39,10 @@ boost::json::object serialize_primitive(const Primitive &primitive) {
         return {{"Arc", serialize_arc(std::get<RenderCore::Arc>(primitive))}};
     } else if (std::holds_alternative<RenderCore::PenOptions>(primitive)) {
         return {{"PenOptions", serialize_pen_options(std::get<RenderCore::PenOptions>(primitive))}};
+    } else if (std::holds_alternative<RenderCore::Polygon>(primitive)) {
+        return {{"Polygon", serialize_polygon(std::get<RenderCore::Polygon>(primitive))}};
+    } else if (std::holds_alternative<RenderCore::Rectangle>(primitive)) {
+        return {{"Rectangle", serialize_rectangle(std::get<RenderCore::Rectangle>(primitive))}};
     }
     return {};
 }
