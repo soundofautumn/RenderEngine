b706b0f7103ac9d2ae2768445446e41543f896b3
soundofautumn <qjm1412@163.com>
2024-09-20 18:36:55 +0800

    feat: 更新测试

--- src/engine/test.cpp
+++ src/engine/test.cpp
@@ -8,10 +8,27 @@
 
 using namespace RenderCore;
 
+static RenderEngine engine;
+
+void lab_1();
+void lab_2();
+
 int main() {
-    RenderEngine engine;
     engine.init(800, 600);
     engine.clear();
+
+    // lab_1();
+    lab_2();
+
+    engine.render();
+    engine.save("output.bmp");
+#ifdef _WIN32
+    system("mspaint output.bmp");
+#endif
+    return 0;
+}
+
+void lab_1() {
     engine.set_pen_options({.color = Colors::Red, .type = PenOptions::LineType::DASH});
     engine.add_primitive(make_line({0, 0}, {799, 599}));
     engine.set_pen_options({.color = Colors::Green, .type = PenOptions::LineType::DASH_DOT});
@@ -24,10 +41,15 @@ int main() {
     engine.add_primitive(make_circle_center_radius({400, 300}, 200));
     engine.set_pen_options({.color = Colors::Cyan});
     engine.add_primitive(make_arc_center_radius_angle({400, 300}, 200, 0, std::numbers::pi / 2));
-    engine.render();
-    engine.save("output.bmp");
-#ifdef _WIN32
-    system("mspaint output.bmp");
-#endif
-    return 0;
+}
+
+void lab_2() {
+    // 矩形绘制
+    engine.add_primitive(make_rectangle({100, 100}, {200, 200}, Rectangle::Action::Draw));
+    // 多边形绘制
+    // engine.add_primitive(make_polygon({{300, 100}, {400, 100}, {350, 200}}, Polygon::Action::Draw));
+    // 区域填充
+    // engine.add_primitive(make_polygon({{500, 100}, {600, 100}, {550, 200}}, Polygon::Action::Clip));
+    // 矩形裁剪
+    // engine.add_primitive(make_rectangle({100, 300}, {200, 400}, Rectangle::Action::Clip));
 }
