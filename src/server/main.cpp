7d85474b96fec15d9081de19658a41469c8c5a72
soundofautumn <qjm1412@163.com>
2024-09-26 09:24:22 +0800

    feat: 添加命令行选项

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,26 +1,37 @@
-
-#include <csignal>
+#include <boost/program_options.hpp>
+#include <iostream>
 #include <thread>
 
 #include "Listener.h"
 #include "logger.h"
 
-void handle_signal(int signal) {
-    if (signal == SIGINT) {
-        spdlog::info("Received SIGINT, shutting down...");
-        spdlog::shutdown();
-        std::exit(0);
-    }
-}
+namespace po = boost::program_options;
 
-int main() {
+int main(int argc, char *argv[]) {
     init_logger();
 
-    std::signal(SIGINT, handle_signal);
+    // 定义一个描述命令行参数的对象
+    po::options_description desc("Allowed options");
+    desc.add_options()("help,h", "produce help message")(
+        "port,p", po::value<unsigned short>()->default_value(3000), "set port number")(
+        "address,a", po::value<std::string>()->default_value("0.0.0.0"), "set server address")(
+        "threads,t", po::value<int>()->default_value(4), "set number of threads");
 
-    auto const address = boost::asio::ip::make_address("0.0.0.0");
-    auto const port = static_cast<unsigned short>(3000);
-    auto const threads = 4;
+    // 存储命令行参数的变量
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);  // 通知并解析参数
+
+    // 处理 --help 选项
+    if (vm.count("help")) {
+        desc.print(std::cout);
+        return 0;
+    }
+
+    // 获取参数值
+    auto const address = boost::asio::ip::make_address(vm["address"].as<std::string>());
+    auto const port = vm["port"].as<unsigned short>();
+    auto const threads = vm["threads"].as<int>();
 
     logger::info("Server started");
 
@@ -31,6 +42,12 @@ int main() {
 
     // Create and launch a listening port
     std::make_shared<Listener>(ioc, tcp::endpoint{address, port})->run();
+
+    boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](boost::system::error_code const &, int signal_number) {
+        spdlog::info("Received signal {}, shutting down...", signal_number);
+        ioc.stop();  // 停止 io_context
+    });
 
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
