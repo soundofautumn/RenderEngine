c1f2b31060400ecf7c94cb2695ba06268f7126fc
soundofautumn <qjm1412@163.com>
2024-10-14 16:30:35 +0800

    fix: 编译错误

--- src/server/serialize/serialize.h
+++ src/server/serialize/serialize.h
@@ -7,7 +7,14 @@
 
 #include <boost/json.hpp>
 
+#include "bezier.hpp"
+#include "circle.hpp"
+#include "fill.hpp"
+#include "line.hpp"
+#include "options.hpp"
+#include "polygon.hpp"
 #include "primitive.hpp"
+#include "rectangle.hpp"
 #include "serialize_bezier.h"
 #include "serialize_circle.h"
 #include "serialize_fill.h"
@@ -16,6 +23,7 @@
 #include "serialize_polygon.h"
 #include "serialize_rectangle.h"
 #include "serialize_transform.h"
+#include "transform.hpp"
 
 inline RenderCore::Primitive deserialize_primitive(const boost::json::object &primitive) {
     if (primitive.contains("Line")) {
